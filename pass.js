99dc59528894e28ef09a6117f9c0b9b1c7905e30
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2024-12-22 12:18:58 -0800

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,13 @@
+function ck(e) {
+if (e.key.toLowerCase() === "enter") {
+mainPage()
+}
+}
+
+function mainPage() {
+  let textbox = document.querySelector("#i")
+  let textp = textbox.value
+  if (textp === "I_Am_A_Smart_Programmer") {
+    
+  }
+}
