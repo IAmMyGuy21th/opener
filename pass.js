9ee9453b08aabc855c82c094d6f16a7a363d4cae
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2024-12-22 12:21:28 -0800

    Update pass.js

--- pass.js
+++ pass.js
@@ -6,8 +6,13 @@ mainPage()
 
 function mainPage() {
   let textbox = document.querySelector("#i")
+  let button = document.querySelector("#s")
+  let divs = document.querySelectorAll(".center")
   let textp = textbox.value
   if (textp === "I_Am_A_Smart_Programmer") {
+    textbox.remove()
+    button.remove()
+    divs.remove()
     
   }
 }
