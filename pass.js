a6ae4718da362dcf34f73b07af11832b06e4eb5d
Hisham <159368595+IAmMyGuy21th@users.noreply.github.com>
2024-12-22 18:24:02 -0800

    Update pass.js

--- pass.js
+++ pass.js
@@ -8,7 +8,7 @@ function mainPage() {
   let textbox = document.querySelector("#i")
   let button = document.querySelector("#s")
   let divs = document.querySelectorAll(".center")
-  let headings = document.querySelectorAll("h1")
+  let headings = document.querySelector("#h")
   let textp = textbox.value
   if (textp === "I_Am_A_Smart_Programmer") {
     textbox.remove()
@@ -20,6 +20,6 @@ function mainPage() {
     iframe.width = '100%'
     iframe.height = '100%'
     iframe.style = 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'
-    document.body.appendChild('iframe')
+    document.body.appendChild(iframe)
   }
 }
